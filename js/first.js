68642071d2cff439480f1ce922394c28bf045bf7
phyuna0525 <128463899+phyuna0525@users.noreply.github.com>
2023-10-25 23:28:27 +0900

    이미지 슬라이더

---
+++ js/first.js
@@ -0,0 +1,50 @@
+const slides1 = document.querySelector("#slideShow1 .img-container");
+const sliderImg1 = document.querySelectorAll("#slideShow1 .img-container li");
+let currentIdx1 = 0;
+const slideCount1 = sliderImg1.length;
+const before1 = document.querySelector(".before");
+const after1 = document.querySelector(".after");
+const arrowWrap = document.querySelector(".arrow-wrap");
+
+before1.addEventListener("click", function () {
+  if (currentIdx1 > 0) {
+    currentIdx1--;
+    moveSlide1(currentIdx1);
+  }
+});
+
+after1.addEventListener("click", function () {
+  if (currentIdx1 < slideCount1 - 1) {
+    currentIdx1++;
+    moveSlide1(currentIdx1);
+  }
+});
+
+function moveSlide1(idx) {
+  slides1.style.left = -idx * 100 + "%";
+  currentIdx1 = idx;
+}
+
+after1.addEventListener("mouseenter", () => {
+  arrowWrap.style.opacity = 1;
+});
+
+after1.addEventListener("mouseleave", () => {
+  arrowWrap.style.opacity = 0;
+});
+
+before1.addEventListener("mouseenter", () => {
+  arrowWrap.style.opacity = 1;
+});
+
+before1.addEventListener("mouseleave", () => {
+  arrowWrap.style.opacity = 0;
+});
+
+slides1.addEventListener("mouseenter", () => {
+  arrowWrap.style.opacity = 1;
+});
+
+slides1.addEventListener("mouseleave", () => {
+  arrowWrap.style.opacity = 0;
+});
---
+++ js/third.js
@@ -0,0 +1,50 @@
+const slides3 = document.querySelector("#slideShow3 .img-container");
+const sliderImg3 = document.querySelectorAll("#slideShow3 .img-container li");
+let currentIdx3 = 0;
+const slideCount3 = sliderImg3.length;
+const before3 = document.querySelector(".before3");
+const after3 = document.querySelector(".after3");
+const arrowWrap3 = document.querySelector(".arrow-wrap3");
+
+before3.addEventListener("click", function () {
+  if (currentIdx3 > 0) {
+    currentIdx3--;
+    moveSlide3(currentIdx3);
+  }
+});
+
+after3.addEventListener("click", function () {
+  if (currentIdx3 < slideCount3 - 1) {
+    currentIdx3++;
+    moveSlide3(currentIdx3);
+  }
+});
+
+function moveSlide3(idx) {
+  slides3.style.left = -idx * 100 + "%";
+  currentIdx3 = idx;
+}
+
+after3.addEventListener("mouseenter", () => {
+  arrowWrap3.style.opacity = 1;
+});
+
+after3.addEventListener("mouseleave", () => {
+  arrowWrap3.style.opacity = 0;
+});
+
+before3.addEventListener("mouseenter", () => {
+  arrowWrap3.style.opacity = 1;
+});
+
+before3.addEventListener("mouseleave", () => {
+  arrowWrap3.style.opacity = 0;
+});
+
+slides3.addEventListener("mouseenter", () => {
+  arrowWrap3.style.opacity = 1;
+});
+
+slides3.addEventListener("mouseleave", () => {
+  arrowWrap3.style.opacity = 0;
+});
